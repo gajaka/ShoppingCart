0cd0badda4f2d777e538eca821495b01c124b48b
Dragan Stosic <dragan.stosic@gmail.com>
2017-03-22 12:23:39 +0000

    Update CheckOut.scala
    refactoring

--- src/main/scala/fruits/CheckOut.scala
+++ src/main/scala/fruits/CheckOut.scala
@@ -1,6 +1,6 @@
 package fruits
 
-abstract class Fruit (val price: BigDecimal = 0.0, val name:String)
+abstract class Fruit (val price: BigDecimal = 0.0, val name:String = "")
 
 object Apple extends Fruit(0.6, "Apple")
 
@@ -8,15 +8,10 @@ object Orange extends Fruit(0.25, "Orange")
 
 trait ShoppingCart {
 
-  def accumulate: Iterable[BigDecimal] => BigDecimal = {
-    case l => l.foldLeft[BigDecimal](0)((x, y) => x + y)
-  }
-
   def checkOut: BigDecimal
 
-
   def printSum (fruits: Iterable[Fruit],sum: BigDecimal) {
-    val format = "[" + (fruits :\ List[String]())((i,l) => i.name :: l).mkString(",") + "] => "  ;
+    val format = "[" + (fruits :\ List[String]())((i,l) => i.name :: l).mkString(",") + "] => "
     sum match {
       case s if s < 1.0 => println(format  + s.toString +  "p")
       case s if s >= 1.0 => println( format +  "£" + s.toString)
@@ -27,17 +22,17 @@ trait ShoppingCart {
 object ShoppingCart {
 
   private case class ShoppingCartStandard
-  (val fruits: Iterable[Fruit]) extends ShoppingCart {
+  (fruits: Iterable[Fruit]) extends ShoppingCart {
 
     /** Step 1
       *
       * @return sum of all elements
       */
-    override def checkOut = accumulate(fruits.map(_.price));
+    override def checkOut = fruits.map(_.price).sum
   }
 
   private case class ShoppingCartOffer
-  (val fruits: Iterable[Fruit]) extends ShoppingCart {
+  (fruits: Iterable[Fruit]) extends ShoppingCart {
     /**
       * Step 2
       *
@@ -51,24 +46,23 @@ object ShoppingCart {
       val prices = fruits.groupBy(identity).map {
 
         case (f, l) if f.name == Apple.name =>
-          accumulate(l.map(_.price).zipWithIndex
-            .collect { case (e, i) if ((i + 1) % 2) != 0 => e })
+          l.map(_.price).zipWithIndex
+            .collect { case (e, i) if ((i + 1) % 2) != 0 => e }.sum
 
         case (f, l) if f.name == Orange.name =>
-          accumulate(l.map(_.price).zipWithIndex
-            .collect { case (e, i) if ((i + 1) % 3) != 0 => e })
+          l.map(_.price).zipWithIndex
+            .collect { case (e, i) if ((i + 1) % 3) != 0 => e }.sum
       }
-      accumulate(prices)
+      prices.sum
     }
   }
 
-  def apply (strategy: String, l: Iterable[Fruit])={
+  def apply (strategy: String, l: Iterable[Fruit])=
     strategy match
     {
       case "OFFER" => ShoppingCartOffer(l)
       case _ => ShoppingCartStandard(l)
     }
-  }
 
 }
 
